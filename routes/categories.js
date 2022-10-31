2557504636a665f2a7b1a14d8e5dd9c79d3f7eb5
Miguel <mirkouda@gmail.com>
2022-10-31 17:20:44 +0100

    endpoints second round

--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const CategoryController = require('../controllers/CategoryController')
 
 router.post('/createCategory',CategoryController.createCategory)
+router.get("/getCategories",CategoryController.getCategories)
 router.get("/getCategoryById/:id",CategoryController.getCategoryById)
+router.get("/getCategoryByName/:name", CategoryController.getCategoryByName)
+// router.put("/updateCategory", CategoryController) //FALTA ESTA
+// router.delete("/deleteCategoryById/:id", CategoryController,delete)
 
 module.exports = router;
