e2470a5477c7dfbffea862dee08555aec94d8a09
Miguel <mirkouda@gmail.com>
2022-10-31 12:57:15 +0100

    table products created

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-probando
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log('Server started successfully in port ' + PORT);
+  });
+  
