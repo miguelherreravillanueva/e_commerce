5c15bb745c9efda199b5fbe898b7fc2dac6b3c82
Miguel <mirkouda@gmail.com>
2022-11-02 16:44:24 +0100

    category table created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,14 @@
-const { User, Products } = require('../models/index.js');
-
+const { User } = require('../models/index.js');
 const bcrypt = require('bcryptjs');
 
 const UserController = {
-    create(req, res) {
+    createUser(req, res) {
         req.body.role = "user";
         const password = bcrypt.hashSync(req.body.password, 10)
         User.create({ ...req.body, password: password })
-            .then(user => res.status(201).send({ message: 'Usuario creado con éxito', user }))
+            .then(user => res.status(201).send({ message: 'User successfully created', user }))
             .catch(console.error)
     },
 }
+
+module.exports = UserController;
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const UserController = require("../controllers/UserController");
+const { authentication } = require("../middlewares/authentication");
+const router = express.Router()
+
+router.post("/createUser",UserController.createUser)
